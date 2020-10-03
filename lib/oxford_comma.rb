2cfada641266ad5f2c68faa734571bb94f72754e
madelinemc <madeline.coons@gmail.com>
2020-10-03 15:13:43 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+#array = ["string", "string", "string"]
 
+def oxford_comma(array)
+    if array.length == 1
+        return array.join
+#returns string without additional formatting when given 1 element array
+    elsif array.length == 2
+        return array.join("and ")
+        #adds and between elements when given a 2-element array
+    elsif array.length == 3
+        return array.join
+        #adds commas plus a final and when given a 3 element array 
+    else
+        return array.join
+        #correctly formats arrays of lengths greater than 3
 end
