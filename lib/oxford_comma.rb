e9933df291de4d50339064e84a0769903e038502
madelinemc <madeline.coons@gmail.com>
2020-10-03 16:13:08 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,14 +3,13 @@
 def oxford_comma(array)
     if array.length == 1
         return array.join
-#returns string without additional formatting when given 1 element array
+        #returns string without additional formatting when given 1 element array
     elsif array.length == 2
-        return array.join("and ")
+        return array.join(" and ")
         #adds and between elements when given a 2-element array
-    elsif array.length == 3
-        return array.join
-        #adds commas plus a final and when given a 3 element array 
-    else
-        return array.join
-        #correctly formats arrays of lengths greater than 3
+    else array.length >= 3
+        last_string_item = array.pop
+        return array.join(", ") << (", and ") + last_string_item 
+        #adds commas plus a final and when given a 3+ element array 
+    end
 end
